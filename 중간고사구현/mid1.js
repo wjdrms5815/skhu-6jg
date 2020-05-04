f8b3a930e009e4084a1e30ce1f295baa35cb954f
Yuk, jeonggeun <wjdrms5815@gmail.com>
2020-05-04 09:21:16 +0900

    commit

---
+++ 중간고사구현/mid1.js
@@ -0,0 +1,12 @@
+let a = [];
+let sum=0;
+let aver=0;
+    for(let i=0; i<=100; i++){
+        a[i] = Math.floor(Math.random()*100+1);
+        sum += a[i]
+        aver = sum/100}
+
+        a.sort()
+
+    console.log(a);
+    console.log(aver);
---
+++ 중간고사구현/mid2.js
@@ -0,0 +1,4 @@
+let a = [];
+
+    for(let i=0; i<=100; i++){
+        a[i] = Math.floor(Math.random()*100+1);
