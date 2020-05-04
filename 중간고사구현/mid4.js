f03685dee338e81f62e80bfd72494984b5adcccf
Yuk, jeonggeun <wjdrms5815@gmail.com>
2020-05-04 11:00:48 +0900

    commit

--- 중간고사구현/mid4.js
+++ 중간고사구현/mid4.js
@@ -1,8 +1,16 @@
 function sum(arr){
     let result = 0.0;
-
     for(let i=0; i<arr.length; i++)
     result+=arr[i];
-    
     return result;
 }
+
+function test_sum() {
+    let arr = [];
+    for(let i= 0 ;i<=5;i++){
+        arr[i] = Math.floor(Math.random()*10+1);}
+    console.log(arr);
+    console.log(sum(arr));
+}
+    for(let i = 0 ; i<=5;i++)
+    test_sum();
--- 중간고사구현/mid5.js
+++ 중간고사구현/mid5.js
@@ -1,5 +1,5 @@
 let a = [];
-
     for(let i=0; i<=100; i++){
         a[i] = Math.floor(Math.random()*100+1);}
-        
+        a.sort()
+let sum [] = a.filter((e)=>e%2===0);
